a003ea429f799fa0a2f89cb78b00982ca4ae5852
Emerson Carvalho <emerson.acba@gmail.com>
2025-05-13 21:51:19 -0300

    Atualizando

--- src/app/components/main-menu/main-menu.component.ts
+++ src/app/components/main-menu/main-menu.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AfterViewInit, Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,8 +8,29 @@ import { Router } from '@angular/router';
   templateUrl: './main-menu.component.html',
   styleUrl: './main-menu.component.css'
 })
-export class MainMenuComponent {
-  constructor(private router: Router) {}
+export class MainMenuComponent implements AfterViewInit {
+  constructor(private http:HttpClient, private router:Router){}
+
+  isLoggedIn = localStorage.getItem('loggedIn');
+  logado:Boolean = false;
+  mainImageVehicle:string = "";
+
+  ngAfterViewInit(): void {
+    this.setImagem();
+    if (this.isLoggedIn === 'true'){
+      this.logado = true;
+    } else {
+      this.router.navigate(["/signin"]);
+    }
+  }
+
+  LogTheUserOut(){
+    localStorage.setItem('loggedIn', 'false');
+    this.logado = false;
+    this.isLoggedIn = 'false';
+    this.router.navigate(["/signin"]);
+
+  }
 
   goToDashboard() {
     this.router.navigate(['/dashboard']);
@@ -21,4 +43,17 @@ export class MainMenuComponent {
   goToLogin() {
     this.router.navigate(['/signin']);
   }
+
+  setImagem() {
+  this.http.get<any[]>("http://localhost:3001/vehicles").subscribe({
+    next: (res) => {
+      this.mainImageVehicle = res[0].img;
+      console.log(this.mainImageVehicle)
+    },
+    error: (err) => {
+      console.error("Erro ao buscar veículos:", err);
+    }
+  });
 }
+}
+
--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -14,10 +14,14 @@ import { Router} from '@angular/router';
   styleUrl: './signin.component.css'
 })
 export class SigninComponent implements AfterViewInit {
-  constructor(private http:HttpClient, private routes:Router){}
+  constructor(private http:HttpClient, private router:Router){}
 
   ngAfterViewInit(): void {
-    this.focusEmail()
+    this.setImagem();
+    this.focusEmail();
+    if (this.isLoggedIn === "true"){
+      this.router.navigate(['/']);
+    }
   }
   
   @ViewChild('emailInput') inputEmail!: ElementRef;
@@ -33,17 +37,29 @@ export class SigninComponent implements AfterViewInit {
 
 
   showPopupError(){    
+    //Este popup é um popup informativo e não necessariamente de erro. Em caso de credenciais erradas, exibe uma mensagem de erro (esta validação está no HTML com condicional if). Em caso de sucesso, exibe mensagem de sucesso.
 
     const popupError = new bootstrap.Modal(this.popupError.nativeElement);
     popupError.show();
-    this.verificarUser();   
+
   }
+
+ hidePopup() {
+  const popupElement = document.getElementById("popupError");
+  if (popupElement) {
+    popupElement.style.display = "none";
+  }
+}
+
 
   credenciaisCorretas:Boolean = false;
   email = '';
   senha = '';
+  mainImageVehicle:string = "";
+  remember = document.getElementById("rememberMe") as HTMLInputElement;
+  isLoggedIn = localStorage.getItem('loggedIn');
+;
 
-  
   focusEmail() {
     if (this.email.trim() === '') {
       this.inputEmail?.nativeElement.focus();
@@ -54,41 +70,50 @@ export class SigninComponent implements AfterViewInit {
   }
   
   validarCampos(): Boolean {
+    //Função que vai verificar se os dados de e-mail e senha estão preenchidos. Retornará um booleano
     return this.email.trim() !== '' && this.senha.trim() !== '';
   }
 
-  verificarUser(){
-    
-    if (!this.validarCampos()){
-      this.credenciaisCorretas = false;
-    } else if (this.email == "admin" && this.senha == "123456"){
-      this.credenciaisCorretas = true;
-      this.soltarConfete()
-    } else {
-      this.credenciaisCorretas = false;
-
-    } 
-  }
-
+  //Função que será chamada no botão "entrar". Vai fazer uma requisição API para verificar se as credenciais estão corretas.
   conferirDados():void {
     this.http.post("http://localhost:3001/login",{nome:this.email,senha:this.senha},{
       headers: new HttpHeaders({"Accept":"application/json"}), 
       withCredentials : false
     }).subscribe({
       next: (res) => {
-        console.log("oi")
         this.credenciaisCorretas = true;
+        localStorage.setItem('loggedIn', 'true');
         this.soltarConfete();
+        this.showPopupError();
+        setTimeout(() => {
+        this.hidePopup();
+        this.router.navigate(['/']);
+        }, 2000); 
+        
       },
       error:(err) => {
         this.credenciaisCorretas = false;
+        this.showPopupError();
       }
     })
   }
 
+  //Função que vai gravar a imagem inicial da Ford ranger na tela
+  // Função que vai gravar a imagem inicial da Ford Ranger na tela
+setImagem() {
+  this.http.get<any[]>("http://localhost:3001/vehicles").subscribe({
+    next: (res) => {
+      this.mainImageVehicle = res[0].img;
+    },
+    error: (err) => {
+      console.error("Erro ao buscar veículos:", err);
+    }
+  });
+}
+
 
   soltarConfete() {
-    const duration =  500;
+    const duration =  1500;
     const end = Date.now() + duration;
 
     (function frame() {
