8cb9bda6e6b5a6ae097095d01e927a2a9cdaa099
Emerson Carvalho <emerson.acba@gmail.com>
2025-05-14 11:57:35 -0300

    Atualizando

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MainMenuComponent } from '../main-menu/main-menu.component';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -12,22 +14,74 @@ import { MainMenuComponent } from '../main-menu/main-menu.component';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements AfterViewInit {
- 
-  vehicleImages = [
-    'assets/Ranger Farol Desligado.png',
-    'assets/Mustang Farol Desligado.png',
-    'assets/Territory Farol Desligado.png',
-    'assets/Bronco Farol Desligado.png'
-];
+  constructor(private http:HttpClient, private router:Router){}
+  vehicleModel:string = "";
+  vehicleVolumetotal:number = 0;
+  vehicleConnected:number = 0;
+  vehicleSoftwareUpdates:number = 0;
+  vehicleVin:string = "";
+  vehicleImg:string = "";
+  vehicleOdometro:number = 0;
+  vehicleNivelCombustivel:number = 0;
+  vehicleStatus:string = "-";
+  vehicleLat:number = 0;
+  vehicleLong:number = 0;
+  vinInput:string = "-"
+
+
+
+
+
 
   @ViewChild('dropdownVehicle') selectVehicle!: ElementRef;
-  selectedVehicle: number = 1;
+  selectedVehicle: number = 0;
 
   ngAfterViewInit() {
-    this.selectedVehicle = Number(this.selectVehicle.nativeElement.value);
+    this.setImagem();
   }
 
   getSelectedVehicle() {
-    this.selectedVehicle = Number(this.selectVehicle.nativeElement.value);
+    this.selectedVehicle = Number(this.selectVehicle.nativeElement.value)-1;
+    console.log(this.selectedVehicle);
+    this.setImagem()
+  }
+
+  getChangeVin(){
+    const input = document.getElementById("inputVin") as HTMLInputElement;
+    this.vinInput = input.value;
+    this.setVehicleInfo(this.vinInput)
+    console.log("Entrou no change e enviando")
+    console.log(this.vinInput)
+  }
+
+  setImagem() {
+  this.http.get<any[]>("http://localhost:3001/vehicles").subscribe({
+    next: (res) => {
+      this.vehicleImg = res[this.selectedVehicle].img;
+      this.vehicleModel = res[this.selectedVehicle].vehicle;
+      this.vehicleVolumetotal = res[this.selectedVehicle].volumetotal;
+      this.vehicleConnected = res[this.selectedVehicle].connected;
+      this.vehicleSoftwareUpdates = res[this.selectedVehicle].softwareUpdates;
+      this.vehicleVin = res[this.selectedVehicle].vin;
+    },
+    error: (err) => {
+      console.error("Erro ao buscar veículos:", err);
+    }
+  });
+  }
+
+  setVehicleInfo(vin:String) {
+  this.http.post<any>("http://localhost:3001/vehicleData",{vin}).subscribe({
+    next: (res) => {
+      this.vehicleOdometro = res.odometro;
+      this.vehicleNivelCombustivel = res.nivelCombustivel;
+      this.vehicleStatus = res.status;
+      this.vehicleLat = res.lat;
+      this.vehicleLong = res.long;
+    },
+    error: (err) => {
+      console.error("Erro ao buscar veículos:", err);
+    }
+  });
   }
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { MainMenuComponent } from '../main-menu/main-menu.component';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Modal } from 'bootstrap';
 
 @Component({
   selector: 'app-home',
@@ -8,8 +11,29 @@ import { MainMenuComponent } from '../main-menu/main-menu.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit{
+  constructor(private http:HttpClient, private router:Router){}
+  mainImageVehicle:string = "";
 
+
+  ngAfterViewInit(): void {
+    this.setImagem();
+    const popup = document.getElementById("elementPopupInfo") as HTMLElement;
+    const popupInfo = new Modal(popup);
+    popupInfo.show();
+  }
+
+  setImagem() {
+  this.http.get<any[]>("http://localhost:3001/vehicles").subscribe({
+    next: (res) => {
+      this.mainImageVehicle = res[0].img;
+      console.log(this.mainImageVehicle)
+    },
+    error: (err) => {
+      console.error("Erro ao buscar veículos:", err);
+    }
+  });
+}
   
 
 }
